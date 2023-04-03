14b8b106aa014399acb8242427742ef7d4dd0f8e
David Anderle <86005883+Andergonan@users.noreply.github.com>
2023-04-03 08:50:16 +0200

    Add files via upload

---
+++ usergroup.sh
@@ -0,0 +1,21 @@
+# kontrola počtu argumentů
+if [ "$#" -ne 2 ]; then
+  echo "Chyba: Musi byt zadany dva argumenty - nazevUzivatele nazevSkupiny"
+  exit 1
+fi
+
+# vytvoření skupiny
+sudo groupadd $2
+
+# vytvoření uživatele
+sudo useradd -m $1
+
+# přidání uživatele do skupiny
+sudo usermod -a -G $2 $1
+
+echo "Uživatel $1 vytvořen a přidán do skupiny $2"
+
+# ------------------------------------------
+# -_-_-_-_--_-_-_-_-volání-_-_-_-_--_-_-_-_-
+# ./usergroup.sh jmenoUzivatele jmenoSkupiny
+#-_-_-_-_--_-_-_-_--_-_-_-_--_-_-_-_--_-_-_-
